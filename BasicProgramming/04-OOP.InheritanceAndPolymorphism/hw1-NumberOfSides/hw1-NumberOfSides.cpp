45646cc4a23f7f8a5e12568754a807247e43b558
Назаров Сергей <nazarov.sa@mail.ru>
2025-05-19 16:19:31 +0300

    Создан проект hw1-NumberOfSides

---
+++ BasicProgramming/04-OOP.InheritanceAndPolymorphism/hw1-NumberOfSides/hw1-NumberOfSides.cpp
@@ -0,0 +1,39 @@
+﻿// hw1-NumberOfSides.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <locale>
+
+class Parent
+{
+public:
+    Parent(int a)
+    {
+        std::cout << "Создаю родителя " << a << " \n";
+    }
+};
+
+class Child : public Parent
+{
+public:
+    Child() : Child(5)
+    {
+        std::cout << "Создаю потомка\n";
+    }
+    Child(int a) : Parent(a)
+    {
+        std::cout << "Создаю потомка " << a << " \n";
+    }
+};
+int main(int argc, char** argv)
+{
+    setlocale(LC_ALL, "russian");
+
+    Parent parent(3);  // Создаю родителя 3
+     Child child_empty; // Создаю родителя 5
+                       // Создаю потомка 5
+                       // Создаю потомка
+    Child child_8(8);  // Создаю родителя 8
+                       // Создаю потомка 8
+}
+
