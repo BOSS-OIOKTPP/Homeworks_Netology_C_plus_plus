5b8a72c8f960e9f81361dee6135afced92f9799d
Sergey Nazarov <nazarov.sa@mail.ru>
2025-05-22 22:49:57 +0300

    Продолжили работу над проектом

--- BasicProgramming/05-MultifileProjects/hw1-MathematicalFunctions/hw1-MathematicalFunctions.cpp
+++ BasicProgramming/05-MultifileProjects/hw1-MathematicalFunctions/hw1-MathematicalFunctions.cpp
@@ -1,20 +1,38 @@
 ﻿// hw1-MathematicalFunctions.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 //
+//Задача 1. Математические функции
+//В этом задании вы вынесете математические функции в отдельный файл.
+//
+//Даны следующие математически функции : сложение, вычитание, умножение, деление, возведение в степень.
+//
+//Вам нужно разместить их в отдельном файле исходного кода и воспользоваться ими из функции main.
+//
+//Попросите пользователя ввести два числа и выбрать операцию.Выведите результат ему на консоль.
+//
+//Пример работы программы
+//Консоль
+//Введите первое число : 20
+//Введите второе число : 2
+//Выберите операцию(1 - сложение, 2 вычитание, 3 - умножение, 4 - деление, 5 - возведение в степень) : 5
+//20 в степени 2 = 400
 
 #include <iostream>
 #include <locale>
 #include <string>
 #include "MathFunctions.h"
 
-class NumPair {
+class TNumPair {
 private:
     int FA;          // 1 число
     int FB;          // 2 число
     int FOperation;  // номер операции над числами: 1 - сложение, 2 вычитание, 3 - умножение, 4 - деление, 5 - возведение в степень
 public:
-    
-    // Метод устанавливает значение поля num1 равное значению аргумента num1 в случае, если значение аргумента не равно 0. 
-    // Возвращает true, если установка произошла успешно, в обратном случае false
+    TNumPair() {
+        FA = 0;
+        FB = 0;
+        FOperation = 0;
+    }
+
     void SetA(int A) { FA = A; }
     int GetA() { return FA; }
 
@@ -25,8 +43,30 @@ public:
     int GetOperation() { return FOperation; }
 
     bool Checked(std::string& Err) {
-        return false;
-    }       
+        bool ch(true);
+        Err = "";
+        if (FOperation == 4 && FB == 0) {
+             Err = "Неверный ввод!. Второе число должно быть не равно 0!";
+             ch = false;
+        }
+        else if (FOperation == 5 && FB < 0) {
+            Err = "Неверный ввод!. Второе число должно быть больше или равно 0!";
+            ch = false;
+        }
+        else {
+            Err = "Некорректный номер операции!";
+            ch = false;
+        }
+        return ch;
+    }   
+
+    void Print() {        
+        if (FOperation == 1) { std::cout << FA << " + " << FB << " = " << Add(FA, FB) << std::endl; }
+        else if (FOperation == 2) { std::cout << FA << " - " << FB << " = " << Subtract(FA, FB) << std::endl; }
+        else if (FOperation == 3) { std::cout << FA << " * " << FB << " = " << Multiply(FA, FB) << std::endl; }
+        else if (FOperation == 4) { std::cout << FA << " / " << FB << " = " << Multiply(FA, FB) << std::endl; }
+        else if (FOperation == 5) { std::cout << FA << " в степени " << FB << " = " << Power(FA, FB) << std::endl; }
+    }
 };
 
 
@@ -35,11 +75,15 @@ int main()
 {
     setlocale(LC_ALL, "russian");
 
-    char chContinue{ 'Y' };
-    int A{0};
-    int B{0};
-    int Operation{0};
+    TNumPair NumPair;
     
+    int A{ 0 };
+    int B{ 0 };
+    int Operation{ 0 };
+    std::string Err;
+    bool Checked;
+
+
     bool bChecked{ true };
 
     while (true) {
@@ -49,24 +93,28 @@ int main()
         std::cout << "Введите первое число: ";
         std::cin >> A;
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Очищаем буфер до символа новой строки
+        NumPair.SetA(A);
 
         // Вводим B
         std::cout << "Введите второе число: ";
         std::cin >> B;
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); 
+        NumPair.SetB(B);
 
         // Выбираем операцию
         // Вводим num1
         do {
-            std::cout << "Выберите операцию(1 - сложение, 2 вычитание, 3 - умножение, 4 - деление, 5 - возведение в степень): ";
+            std::cout << "Выберите операцию (1 - сложение, 2 вычитание, 3 - умножение, 4 - деление, 5 - возведение в степень): ";
             std::cin >> Operation;
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            NumPair.SetOperation(Operation);
+
             // сохраняем значение в поле объекта
-            setValue = bChecked;
-            if (setValue == false) {
-                std::cout << "Неверный ввод!" << std::endl;
+            Checked = NumPair.Checked(Err);
+            if (Checked == false) {
+                std::cout << Err << std::endl;
             }
-        } while (setValue == false);
+        } while (Checked == false);
 
         std::cout << "Введите второе число: ";
         std::cin >> B;
