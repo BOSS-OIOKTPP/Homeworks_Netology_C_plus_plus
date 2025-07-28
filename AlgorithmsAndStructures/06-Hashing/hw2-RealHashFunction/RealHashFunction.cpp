9fd6d2c5cbee65f1a279f0878783f4f7783fed35
Назаров Сергей <nazarov.sa@mail.ru>
2025-07-28 16:33:58 +0300

    Д.р. Поиск подстроки

--- AlgorithmsAndStructures/06-Hashing/hw2-RealHashFunction/RealHashFunction.cpp
+++ AlgorithmsAndStructures/06-Hashing/hw2-RealHashFunction/RealHashFunction.cpp
@@ -2,15 +2,25 @@
 #include <string>
 #include <windows.h>
 
-// Вычисляем хэш строки
-int simple_string_hash(std::string S) {
-    int hash{ 0 };
+
+uint64_t real_string_hash(std::string& S, int& p, int& n) {
+    uint64_t hash = 0;
+    int power = 1; // p^0 = 1
     for (unsigned char c : S) {
-        hash += static_cast<int>(c); // Суммируем коды символов
+        hash += static_cast<int>(c) * power;    // Умножаем код символа на p^i и добавляем к хешу
+        power = (power * p) ;                   // Обновляем степень p
     }
+    hash %= n; // Берём остаток
     return hash;
 }
 
+// Проверка на простое число
+bool is_prime(int& p) {
+    for (int i = 2; i <= std::sqrt(p); ++i) {
+        if (p % i == 0)     return false;
+    }
+    return true;
+}
 
 // ГЛАВНАЯ ПРОГРАММА
 int main()
@@ -21,9 +31,37 @@ int main()
 
     std::string input;
     int hash;
+    int p;
+    int n;
 
-    std::cout << "Программа рассчета хэша для строки с помощью наивного алгоритма" << std::endl;
+    std::cout << "Программа рассчета хэша для строки с помощью серьёзного алгоритма" << std::endl;
     std::cout << "Для выхода из программы введите строку: exit" << std::endl << std::endl;
+
+    while (true) {
+        std::cout << "Введите простое число p: ";
+
+        std::cin >> p;
+        std::cin.clear(); // Сбрасываем флаги ошибок, например, если ввели букву
+        std::cin.ignore((std::numeric_limits<std::streamsize>::max)(), '\n'); // Очищаем буфер до символа новой строки
+
+        if (is_prime(p) == true)
+            break;
+        else
+            std::cout << "Ошибка. Число p=" << p << " не является простым!" << std::endl;
+    }
+
+    while (true) {
+        std::cout << "Введите модуль n: ";
+
+        std::cin >> n;
+        std::cin.clear(); // Сбрасываем флаги ошибок, например, если ввели букву
+        std::cin.ignore((std::numeric_limits<std::streamsize>::max)(), '\n'); // Очищаем буфер до символа новой строки
+
+        if (n != 0)
+            break;
+        else
+            std::cout << "Ошибка. Число n должно быть не равно 0." << std::endl;
+    }
 
     while (true) {
         
@@ -38,12 +76,12 @@ int main()
             continue;
         }
 
+        // Вычисляем хэш
+        std::cout << "Хэш строки " << input << " = " << real_string_hash(input, p, n) << std::endl;
+
         if (input == "exit") {
             break;
         }
-
-        // Вычисляем хэш
-        std::cout << "Наивный хэш строки " << input << " = " << simple_string_hash(input) << std::endl;
     }
 
     std::cout << std::endl;
---
+++ AlgorithmsAndStructures/06-Hashing/hw3-Algorithm_Rabin-Karp/Algorithm_Rabin-Karp.cpp
@@ -0,0 +1,106 @@
+﻿#include <iostream>
+#include <string>
+#include <windows.h>
+
+
+// Вычисляем хэш строки
+int simple_string_hash(std::string& S, int start, int length) {
+    int hash{ 0 };
+    for (int i = 0; i < length; ++i) {
+        unsigned char c;
+        c = S[i];
+        hash += static_cast<int>(c); // Суммируем коды символов
+    }
+    return hash;
+}
+
+// Выполняем поиск
+int find_substring_light_rabin_karp(std::string& text, std::string& pattern) {
+    int pattern_length = pattern.size();
+    int text_length = text.size();
+    int pattern_hash = simple_string_hash(pattern, 0, pattern_length);
+
+    for (int i = 0; i <= text_length - pattern_length; ++i) {
+        int current_hash = simple_string_hash(text, i, pattern_length);
+        if (current_hash == pattern_hash) {
+            // Дополнительная проверка на случай коллизии
+            bool match = true;
+            for (int j = 0; j < pattern_length; ++j) {
+                if (text[i + j] != pattern[j]) {
+                    match = false;
+                    break;
+                }
+            }
+            if (match==true) {
+                return i;
+            }
+        }
+    }
+    return -1;
+}
+
+
+
+// ГЛАВНАЯ ПРОГРАММА
+int main()
+{
+    // Установка кодировки консоли Windows
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    std::string text;
+    std::string pattern;
+    int index;
+    
+
+    std::cout << "Программа поиска подстроки в заданной строке с помощью упрощённого алгоритма Рабина-Карпа" << std::endl << std::endl;
+
+    while (true) {
+        std::cout << "Введите строку, в которой будет осуществляться поиск: ";
+        // Чтение всей строки включая пробелы
+        if (!std::getline(std::cin >> std::ws, text)) {
+            // Ошибка ввода (например, Ctrl+Z/Ctrl+D)
+            std::cin.clear(); // Сброс флагов ошибок
+            std::cin.ignore((std::numeric_limits<std::streamsize>::max)(), '\n'); // Очищаем буфер до символа новой строки
+            std::cout << "Ошибка ввода! Попробуйте снова." << std::endl;
+
+            continue;
+        }
+        break;
+    }
+
+    while (true) {
+
+        while (true) {
+            std::cout << "Введите подстроку, которую нужно найти: ";
+            // Чтение всей строки включая пробелы
+            if (!std::getline(std::cin >> std::ws, pattern)) {
+                // Ошибка ввода (например, Ctrl+Z/Ctrl+D)
+                std::cin.clear(); // Сброс флагов ошибок
+                std::cin.ignore((std::numeric_limits<std::streamsize>::max)(), '\n'); // Очищаем буфер до символа новой строки
+                std::cout << "Ошибка ввода! Попробуйте снова." << std::endl;
+
+                continue;
+            }
+            break;
+        }
+
+        if (pattern == "exit") {
+            break;
+        }
+
+        // выполняем поиск
+        index = find_substring_light_rabin_karp(text, pattern);        
+        if (index==-1) 
+            std::cout << "Подстрока " << pattern << " не найдена" << std::endl;
+        else
+            std::cout << "Подстрока " << pattern << " найдена по индексу " << index << std::endl;
+
+    }
+
+    std::cout << std::endl;
+    system("pause");
+
+    return EXIT_SUCCESS;
+}
+
