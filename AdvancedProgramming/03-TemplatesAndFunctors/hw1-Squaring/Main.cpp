ceeaea7fd97d715135707109e3441ee8f2edc440
Назаров Сергей <nazarov.sa@mail.ru>
2025-08-28 16:34:35 +0300

    д.р. по теме шаблоны

---
+++ AdvancedProgramming/03-TemplatesAndFunctors/hw1-Squaring/Main.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <string>
+#include <windows.h>
+
+// Пример использования
+int main() {
+
+    // Установка кодировки консоли Windows
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    std::string input;
+
+    while (true) {
+        // Очищаем консоль 
+        system("cls");
+
+        std::cout << "Введите числа, отделяя их пробелами" << std::endl;
+
+        // Чтение всей строки
+        if (!std::getline(std::cin >> std::ws, input)) {
+            // Ошибка ввода (например, Ctrl+Z/Ctrl+D)
+            std::cin.clear(); // Сброс флагов ошибок
+            std::cin.ignore((std::numeric_limits<std::streamsize>::max)(), '\n'); // Очищаем буфер до символа новой строки
+            std::cout << "Ошибка ввода! Попробуйте снова." << std::endl;
+
+            continue;
+        }
+
+        if (input == "exit") {
+            break;
+        }
+
+        
+    }
+
+    std::cout << std::endl;
+    system("pause");
+
+    return EXIT_SUCCESS;
+}
