3b12d955ad2776b3a4548521f974c34809bebf95
Назаров Сергей <nazarov.sa@mail.ru>
2025-09-05 15:03:52 +0300

    Завершены д.р. по теме STL Часть 2

---
+++ AdvancedProgramming/04-STL_Part2/hw1-UniqueVector/Main.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <string>
+#include <map>
+#include <vector>
+#include <algorithm>
+
+template<typename T>
+void UniqueVector(std::vector<T>& arr) {
+    // 1. Выполняем сортировку по возрастанию
+    std::sort(arr.begin(), arr.end(),
+        [](const auto& a, const auto& b) {
+            return a < b;
+        });
+
+    // 2. Делаем уникальными числа
+    auto it = std::unique(arr.begin(), arr.end());
+
+    // 3. Удаляем элементы, которые остались после применения std::unique
+    arr.erase(it, arr.end());    
+}
+
+int main() {
+    // Установили кодировку
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    // 1. Выводим на экран входные данные
+    std::vector<int> input{ 1, 1, 2, 5, 6, 1, 2, 4 };
+    std::cout << "[IN]: ";
+    for (const int& v : input)
+        std::cout << v << " ";
+    std::cout << std::endl;
+
+    // 2. Делаем уникальный список, отсортированный по возрастанию
+    UniqueVector(input);
+
+    // 3. Выводим на печать результат    
+    std::cout << "[OUT]: ";
+    for (const int& v : input)
+        std::cout << v << " ";
+
+
+    std::cout << std::endl << std::endl;
+    system("pause");
+
+    return EXIT_SUCCESS;
+}
---
+++ AdvancedProgramming/04-STL_Part2/hw2-PrintingContainer/Main.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <set>
+#include <vector>
+#include <list>
+
+
+template<typename T>
+void print_container( T& container ) {
+    bool first = true;
+    for ( auto& element : container ) {
+        if (!first) { std::cout << ", "; }
+        std::cout << element;
+        first = false;
+    }
+    std::cout << std::endl;
+}
+
+int main() {
+    // Установили кодировку
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    std::set<std::string> test_set = { "one", "two", "three", "four" };
+    print_container(test_set); // four one three two
+
+    std::list<std::string> test_list = { "one", "two", "three", "four" };
+    print_container(test_list); // one, two, three, four
+
+    std::vector<std::string> test_vector = { "one", "two", "three", "four" };
+    print_container(test_vector); // one, two, three, four
+
+
+    std::cout << std::endl;
+    system("pause");
+
+    return EXIT_SUCCESS;
+}
