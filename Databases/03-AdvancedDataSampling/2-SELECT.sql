4e380ead7e72a43b6a2c6191ead81dcc9cecb9a4
Назаров Сергей <nazarov.sa@mail.ru>
2025-09-29 10:56:43 +0300

    д.р. по теме Продвинутая выборка данных

--- Databases/03-AdvancedDataSampling/2-SELECT.sql
+++ Databases/03-AdvancedDataSampling/2-SELECT.sql
@@ -53,3 +53,52 @@ from collection c
 	 inner join albumperformer ap on ap.album_id = t.album_id 
 	 inner join performer p on ap.performer_id = p.performer_id
 where p.name = 'Queen'
+
+
+--    Задание 4
+--
+--Названия альбомов, в которых присутствуют исполнители более чем одного жанра.
+select a.name 
+from album a
+where exists(select gp.performer_id, count(*) 
+			   from albumperformer ap  
+			   		inner join genreperformer gp on ap.performer_id =gp.performer_id 
+			   		inner join genre g on gp.genre_id = g.genre_id 
+			   where ap.album_id = a.album_id 
+			   group by gp.performer_id
+			   having count(*)>1
+			   )
+			   
+--Наименования треков, которые не входят в сборники.
+-- версия 1			   
+select t.name
+from track t
+where not exists(select 1 from collectiontrack ct where ct.track_id = t.track_id limit 1)
+
+-- версия 2
+select t.name
+from track t
+	 left join collectiontrack ct on t.track_id = ct.track_id 
+where ct.track_id is null	 
+
+--Исполнитель или исполнители, написавшие самый короткий по продолжительности трек, — теоретически таких треков может быть несколько.
+select p.name
+from track t 
+	 inner join albumperformer ap on t.album_id = ap.album_id 
+	 inner join performer p on ap.performer_id = p.performer_id 
+where t.duration = (select MIN(duration) from track)
+
+
+--Названия альбомов, содержащих наименьшее количество треков.
+select a.name
+from album a
+     inner join track t on a.album_id =t.album_id 
+group by a.album_id
+having count(*) = (
+		select MIN(res.album_track_count)
+		from (select a1.album_id, count(*) album_track_count
+				from album a1
+				     inner join track t1 on a1.album_id =t1.album_id 
+				group by a1.album_id) res
+				)		
+    
