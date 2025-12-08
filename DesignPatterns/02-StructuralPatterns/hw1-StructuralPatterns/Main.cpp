fb98f459ef2f217dbb8add70015257583db3af55
Назаров Сергей <nazarov.sa@mail.ru>
2025-12-08 16:31:21 +0300

    ДЗ: Структурные шаблоны

---
+++ DesignPatterns/02-StructuralPatterns/hw1-StructuralPatterns/Main.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <string>
+
+// Базовый класс
+class Text {
+public:
+    virtual void render(const std::string& data) const {
+        std::cout << data;
+    }
+};
+
+
+class DecoratedText : public Text {
+public:
+    explicit DecoratedText(Text* text) : text_(text) {}
+    Text* text_;
+};
+
+class ItalicText : public DecoratedText {
+public:
+    explicit ItalicText(Text* text) : DecoratedText(text) {}
+    void render(const std::string& data)  const {
+        std::cout << "<i>";
+        text_->render(data);
+        std::cout << "</i>";
+    }
+};
+
+class BoldText : public DecoratedText {
+public:
+    explicit BoldText(Text* text) : DecoratedText(text) {}
+    void render(const std::string& data) const {
+        std::cout << "<b>";
+        text_->render(data);
+        std::cout << "</b>";
+    }
+};
+
+
+
+int main() {
+    // Установка кодировки консоли Windows
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    auto text_block = new ItalicText(new BoldText(new Text()));
+    text_block->render("Hello world");
+
+    std::cout << std::endl;
+    system("pause");
+
+    return EXIT_SUCCESS;
+}
+
