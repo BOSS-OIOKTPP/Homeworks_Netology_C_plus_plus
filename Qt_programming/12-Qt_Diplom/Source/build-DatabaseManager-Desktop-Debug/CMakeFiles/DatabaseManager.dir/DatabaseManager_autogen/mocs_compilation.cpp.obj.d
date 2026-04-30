312c6dfbd34c8d308e3df15dd52fe852c5d1725c
Sergey Nazarov <nazarov.sa@mail.ru>
2026-05-01 00:08:14 +0300

    корректировка

--- Qt_programming/12-Qt_Diplom/Source/build-DatabaseManager-Desktop-Debug/CMakeFiles/DatabaseManager.dir/DatabaseManager_autogen/mocs_compilation.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/DatabaseManager.dir/DatabaseManager_autogen/mocs_compilation.cpp.obj: \
- C:\GIT_PROJECT\Homeworks_Netology_C_plus_plus\Qt_programming\12-Qt_Diplom\Source\build-DatabaseManager-Desktop-Debug\DatabaseManager_autogen\mocs_compilation.cpp
